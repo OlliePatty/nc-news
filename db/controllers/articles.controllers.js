88c6eb91907cc2a557785ff13c093e5dbab4aac6
Oliver Patrick <oliverpatrick@olivers-air.home>
2023-06-27 14:48:00 +0100

    Finished task 5

--- db/controllers/articles.controllers.js
+++ db/controllers/articles.controllers.js
@@ -1,9 +1,16 @@
-const { selectArticlesById } = require('../models/articles.models.js')
+const { selectArticlesById, selectAllArticles } = require('../models/articles.models.js')
 
 exports.getArticlesById = (request, response, next) => {
     const id = request.params.article_id
     selectArticlesById(id).then((article) => {
         response.status(200).send({article})
+    })
+    .catch(next)
+}
+
+exports.getAllArticles = (request, response, next) => {
+    selectAllArticles().then((articles) => {
+        response.status(200).send({articles})
     })
     .catch(next)
 }
