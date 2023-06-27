5fe40ebc962f4b129858b82c1e5d219b557d777c
Oliver Patrick <oliverpatrick@olivers-air.home>
2023-06-27 15:03:49 +0100

    Pulling changes from task 5

--- db/app.js
+++ db/app.js
@@ -2,12 +2,14 @@ const express = require('express')
 const app = express()
 
 const { getAllTopics } = require('./controllers/topics.controllers.js')
-const { getArticlesById } = require('./controllers/articles.controllers.js')
+const { getArticlesById, getCommentsFromArticleId } = require('./controllers/articles.controllers.js')
 const { handlePsqlErrors , handleCustomErrors, handleServerErrors } = require('./errors/errors.js')
 
 app.get('/api/topics', getAllTopics)
 
 app.get('/api/articles/:article_id', getArticlesById)
+
+app.get('/api/articles/:article_id/comments', getCommentsFromArticleId)
 
 app.use(handlePsqlErrors)
 
