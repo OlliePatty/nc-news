603043b39a7ad145e232c28636574d0937492012
Oliver Patrick <oliverpatrick@olivers-air.home>
2023-06-27 19:01:17 +0100

    Formatted the document, but finished task 10

--- db/models/articles.models.js
+++ db/models/articles.models.js
@@ -1,18 +1,23 @@
-const db = require('../connection')
+const db = require("../connection");
 
 exports.selectArticlesById = (id) => {
-    return db.query(`
+  return db
+    .query(`
     SELECT * FROM articles
-    WHERE article_id = $1;`, [id]).then(({rows}) => {
-        if(!rows.length){
-            return Promise.reject({ status: 404, msg: 'Not found'})
-        }
-        return rows[0]
-    })
-}
+    WHERE article_id = $1;`,
+      [id]
+    )
+    .then(({ rows }) => {
+      if (!rows.length) {
+        return Promise.reject({ status: 404, msg: "Not found" });
+      }
+      return rows[0];
+    });
+};
 
 exports.selectAllArticles = () => {
-    return db.query(`
+  return db
+    .query(`
     SELECT 
     author,
     title,
@@ -22,40 +27,45 @@ exports.selectAllArticles = () => {
     votes,
     article_img_url
     FROM articles
-    ORDER BY created_at DESC;`).then((articlesData) => {
-        const articles = articlesData.rows
-        return articles
+    ORDER BY created_at DESC;`
+    )
+    .then((articlesData) => {
+      const articles = articlesData.rows;
+      return articles;
     })
     .then((articles) => {
-        return db.query(`SELECT * FROM comments;`).then((commentsData) => {
-            const comments = commentsData.rows
-            return comments
+      return db
+        .query(`SELECT * FROM comments;`)
+        .then((commentsData) => {
+          const comments = commentsData.rows;
+          return comments;
         })
         .then((comments) => {
-            articles.forEach((articleObj) => {
-                articleObj.comment_count = 0
-            })
-    
-        articles.forEach((article) => {
+          articles.forEach((article) => {
+            article.comment_count = 0;
             comments.forEach((comment) => {
-                if(article.article_id === comment.article_id){
-                    article.comment_count ++
-                }
-            })
-        })
-        return articles
-    })
-    })
-}
+              if (article.article_id === comment.article_id) {
+                article.comment_count++;
+              }
+            });
+          });
+          return articles;
+        });
+    });
+};
 
 exports.selectCommentsFromArticleId = (id) => {
-    return db.query(`
+  return db
+    .query(`
     SELECT * FROM comments
     WHERE article_id = $1
-    ORDER BY created_at DESC;`, [id]).then(({rows}) => {
-        if(!rows.length){
-            return Promise.reject({ status: 404, msg: 'Not found' })
-        }
-        return rows
-    })
-}
+    ORDER BY created_at DESC;`,
+      [id]
+    )
+    .then(({ rows }) => {
+      if (!rows.length) {
+        return Promise.reject({ status: 404, msg: "Not found" });
+      }
+      return rows;
+    });
+};
